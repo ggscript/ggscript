562b940d5a2f42be25f086cc2a53298d353b5ddf
Jeff Judkins <judkinsj@gmail.com>
2016-11-04 15:38:08 -0700

    changed to dev-middleware

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 var express = require('express');
-const app = express();
 
-app.set('port', (process.env.API_PORT || 3001));
 
+var express = require('express');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var app = express();
+ 
+var compiler = webpack(webpackConfig);
+ 
+app.use(express.static(__dirname + '/www'));
+ 
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
 
 app.get('/api/test', function(req, res) {
   console.log('request recieved');
   res.send('success - proxy is working');
 })
-
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+ 
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
-
-/////
