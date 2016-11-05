ba5936bcfcb245b0757ebe9aeae2344c1f1fcc1c
Jason <jmaier@berkeley.edu>
2016-11-05 10:28:29 -0700

    CodeMirror Added

---
+++ src/components/Sandbox.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Sandbox extends Component {
+
+	render() {
+		return (
+				<div>
+					<h1> I am a sandbox bitch! </h1>
+					<textarea className="codemirror-textarea"></textarea>
+				</div>
+			)
+	}
+}
+
+export default Sandbox;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import configureStore from './store/configure-store';
 
 const store = configureStore();
 
+
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
