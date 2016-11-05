e16251c23a091576da3634aeceae3d4684fec00a
Jeff Judkins <judkinsj@gmail.com>
2016-11-05 15:24:40 -0700

    Update SampleApp.js

--- client/app/components/SampleApp.js
+++ client/app/components/SampleApp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import VisibleTodoList from '../containers/VisibleToDoList'
 
 const SampleApp = () => (
   <div>
