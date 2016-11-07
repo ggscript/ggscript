0734e3921dd4b1c2e1ad4b14f24f0425aa3dbdb7
Jeff Judkins <judkinsj@gmail.com>
2016-11-06 17:28:39 -0800

    added initial passing test

--- server/server.js
+++ server/server.js
@@ -34,4 +34,4 @@ var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
-module.exports = app;
+module.exports = server;
