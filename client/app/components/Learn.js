3d0d0ed9a17787517054e8051029994714d98e78
Jeff Judkins <judkinsj@gmail.com>
2016-11-08 00:01:23 -0800

    changed sandbox component to ES6 syntax

--- client/app/components/Learn.js
+++ client/app/components/Learn.js
@@ -8,46 +8,42 @@ require('../../../node_modules/codemirror/addon/hint/javascript-hint.js');
 require('../../../node_modules/codemirror/addon/hint/show-hint.js');
 // require('../../../node_modules/phaser/src/Phaser.js');
 
-export const Sandbox = React.createClass({
-  getInitialState: function() {
-    return {
-      code: "var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create }); \nfunction preload() {\n} \nfunction create() {\n}",
-      phaser: "var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create }); \nfunction preload() {\n} \nfunction create() {\n}"
-    };
-  },
-  updateCode: function(newCode) {
+class Sandbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: "var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create }); \nfunction preload() {\n} \nfunction create() {\n}"
+    }
+  }
+  updateCode(newCode) {
+    console.log(this, 'this')
     this.setState({
       code: newCode
     });
-  },
-  loadCode: function() {
-  	this.setState({
-  		phaser: this.state.code
-  	});
+  }
+
+  loadCode() {
   	document.getElementsByTagName('canvas')[0].remove();
   	document.getElementById('gameScript').remove();
   	const script = document.createElement("script");
   	script.text = this.state.code;
   	script.id = 'gameScript';
   	document.getElementById('gameCode').appendChild(script);
-  },
+  }
+
   componentDidMount() {
   	const script = document.createElement("script");
   	script.id = 'gameScript';
   	script.text = this.state.code;
   	document.getElementById('gameCode').appendChild(script);
-  },
-  componentWillUnmount(){
+  }
+
+  componentWillUnmount() {
   	document.getElementById('gameScript').remove();
   	document.getElementsByTagName('canvas')[0].remove();
-  },
-  // renderPhaser: function(){
-  // 	document.getElementsByTagName('canvas')[0].remove();
-  // 	const script = document.createElement("script");
-  // 	script.text = this.state.phaser;
-  // 	document.getElementById('gameCode').appendChild(script);
-  // },
-  render: function() {
+  }
+
+  render() {
     const options = {
       lineNumbers: true,
       mode: 'javascript',
@@ -60,19 +56,12 @@ export const Sandbox = React.createClass({
     };
     return (
     	<div>
-    		<Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
-    		<button onClick={this.loadCode}> Load Data </button>
+    		<Codemirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
+    		<button onClick={this.loadCode.bind(this)}> Load Data </button>
     		<div id="gameCode"></div>
-    		<Script phaser={this.state.phaser}/>
     	</div>
     	)
   }
-});
+}
 
-export const Script = React.createClass({
-	render(){
-		return(
-			<div></div>
-		)
-	}
-});
+export default Sandbox
