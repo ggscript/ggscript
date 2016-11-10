9d07726a0b7dd224091bba3a7a31f3e2deb351ac
Jason <jmaier@berkeley.edu>
2016-11-10 13:56:52 -0800

    links added between homepage to pertinent pages

--- client/app/components/Home.js
+++ client/app/components/Home.js
@@ -3,22 +3,29 @@ import React from 'react'
 const Home = () => (
   <div className="container">
   <div className="row homepage">
-  	<a href='/#/learn'> <div className="col-md-4">
+  	<a href='/#/learn'> 
+  	<div className="col-md-4">
   		<div id="arcade"></div>
   		<h1 className="text-center">Learn</h1>
 	  	<p className="text-center"> Level up your Phaser skills by completing coding challenges.  
 	  	Available in Novice, Heroic, and Mythic difficulties, which path will you choose? </p>
-	  	</div> </a>
+	</div> 
+	</a>
+	<a href='/#/sandbox'>
   	<div className="col-md-4">
+  		<div id="explore"></div>
 	  	<h1 className="text-center">Explore</h1>
 	  	<p className="text-center"> If you need a greater challenge, use our Phaser sandbox to build your own games from scratch, or with a little guidance from provided templates.</p> 
   	</div>
+  	</a>
+  	<a href='/#/profile'>
   	<div className="col-md-4">
   		<div id="trophy"></div>
 	  	<h1 className="text-center">Brag</h1>
 	  	<p className="text-center"> Visit your player profile page to revisit and share your saved Phaser games.
 	  	You didn't embark on this quest to keep the spoils for yourself did you?  Share with your friends and bask in their adoration.</p>
   	</div>
+  	</a>
   </div>
   </div>
 )
