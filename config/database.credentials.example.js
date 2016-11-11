fbc10c2511de33179ddb2c0709eb47d18bdb706f
Jeff Judkins <judkinsj@gmail.com>
2016-11-11 08:57:32 -0800

    changed database config to hide credentials

---
+++ config/database.credentials.example.js
@@ -0,0 +1,3 @@
+//you should have a file named database.credentials.js that takes the following form
+
+module.exports = "this string contains the login credential url";
---
+++ db/db.js
@@ -0,0 +1,13 @@
+const pg = require('pg');
+var DB_URL;
+if (!process.env.DATABASE_URL) {
+  DB_URL = require('../config/database.credentials');
+}
+console.log(DB_URL)
+
+// Instantiate new pg client (client will read connection from pg cli tools)
+pg.defaults.ssl = true;
+const client = new pg.Client(process.env.DATABASE_URL || DB_URL);
+client.connect();
+
+module.exports = client;
