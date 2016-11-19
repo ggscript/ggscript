853fe7ffc4579118e1ed3e127fd387cbbb936c14
Jeff Judkins <judkinsj@gmail.com>
2016-11-18 19:20:32 -0800

    refactored learn page reducer

--- client/app/reducers/Reducer_GetLevelData.js
+++ client/app/reducers/Reducer_GetLevelData.js
@@ -3,16 +3,8 @@ const getLevelData = (state = {}, action) => {
     case 'LOAD_LEVEL_DATA':
       console.log(action, 'action from reducer');
       console.log(state, 'state');
-      return action
+      return action.data
 
-    case 'UPDATE_STORE':
-      console.log(action, 'action');
-      console.log(state, 'state');
-      return action
-
-    case 'LOAD_LEVEL_POINTS':
-      console.log('in reducer get level data', action.data)
-      return action.data  
 
     default:
       return state
