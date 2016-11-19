8a3054b021c6f3526010becbe934a49bf9eadaee
Jason <jmaier@berkeley.edu>
2016-11-19 10:20:33 -0800

    added modals

--- client/app/containers/Container_App.js
+++ client/app/containers/Container_App.js
@@ -11,12 +11,30 @@ class App extends React.Component {
     this.props.getDisplayName();
   }
   componentWillReceiveProps(nextProps) {
-  
+                      // <NavLink id="logged" to="/login">Log In</NavLink>
+
   }
 
   render() {
     return (
       <div>
+          <div className="modal fade" id="login-modal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="loginmodal-container">
+                  <div className="modal-content">
+                    <div className="modal-body">
+                    <h1 id="makeVideo" className="text-center">Login to Your Account</h1>
+                    <h3 id="makeVideo" className="text-center">Pew Pew, Welcome Back!</h3>
+                    <div className="col-md-8 offset-md-2 modal-footer">
+                      <div className="btn-group btn-group-justified">
+                      <a href="/auth/google" className="btn btn-danger">Google</a>
+                    </div>
+                  </div>
+                </div>
+                </div>
+            </div>
+          </div>
+        </div>
         <nav className="navbar navbar-default">
           <div className="container-fluid">
             <ul className="nav navbar-nav" role="nav">
@@ -36,7 +54,7 @@ class App extends React.Component {
               </ul> : 
               <ul className="nav navbar-nav navbar-right"> 
                 <li>
-                  <NavLink id="logged" to="/login">Log In</NavLink>
+                  <a href='#/' data-target = '#login-modal' data-toggle="modal" className="landing-pg-links"> LOG IN </a>
                 </li>
               </ul>}
           </div>
