63ebe0e9f1f57704a366c3e018267ca0356d6312
Nikki Galusha <nikki@Nikkis-MacBook-Pro.local>
2016-11-21 12:45:30 -0800

    Create second server for ggshell iframe Sandbox

---
+++ ggshell/server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(port);
