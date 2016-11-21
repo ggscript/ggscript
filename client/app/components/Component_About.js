4a848c0909be379124e36195a271760c90b6a7ee
Jason <jmaier@berkeley.edu>
2016-11-21 12:30:06 -0800

    added specific user links to team page

--- client/app/components/Component_About.js
+++ client/app/components/Component_About.js
@@ -31,29 +31,29 @@ class About extends React.Component {
       		</div>
       		<div className="col-md-3">
       			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://lh3.googleusercontent.com/-bIswOe7xSYg/AAAAAAAAAAI/AAAAAAAAAA0/y0_6WkOxIv0/photo.jpg?sz=250"></img>
+      				<img className="img-responsive text-center" src="https://avatars0.githubusercontent.com/u/16483343?v=3&s=250"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Michelle Li </h2>
       			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
       			<div className="col-md-12 text-center">
-		      			<a href="https://github.com/jason-maier" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/maierjason" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+		      			<a href="https://github.com/miteaisgreener" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+		      			<a href="https://www.linkedin.com/in/jing-michelle-li" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
 		      		</div>
       		</div>
       		<div className="col-md-3">
       			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://lh3.googleusercontent.com/-bIswOe7xSYg/AAAAAAAAAAI/AAAAAAAAAA0/y0_6WkOxIv0/photo.jpg?sz=250"></img>
+      				<img className="img-responsive text-center" src="https://avatars2.githubusercontent.com/u/16391503?v=3&s=250"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Nikki Galusha </h2>
       			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
       			<div className="col-md-12 text-center">
-		      		<a href="https://github.com/jason-maier" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/maierjason" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+		      		<a href="https://github.com/nikkigalusha" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+		      			<a href="https://www.linkedin.com/in/nikkigalusha" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
 		      		</div>
       		</div>
       	</div>
       	<div className="row">
-      		<h1> Our Tech Stack </h1>
+      		<h1 id="makeVideo"> Our Tech Stack </h1>
       	</div>
       </div>
       )
