f2b24dbb40e2a184a645d55cfa659fd98178df3c
Jason <jmaier@berkeley.edu>
2016-11-22 17:32:58 -0800

    topic sidebar added

--- client/app/components/Component_Resources.js
+++ client/app/components/Component_Resources.js
@@ -5,10 +5,11 @@ class Resources extends React.Component {
   render() {
     return (
       <div className="container">
+        <h1 id="makeVideo"> Phaser Resources </h1>
       	<div className="row">
-      		<h1 id="makeVideo"> Phaser Resources </h1>
+                  <div className="col-md-10">
                   <br></br>
-                  <h2 id="makeVideo"> Understanding Phaser's Four Core Functions: Preload, Create, Update and Render </h2>
+                  <span id="functions"><h2 id="makeVideo"> Understanding Phaser's Four Core Functions: Preload, Create, Update and Render </h2></span>
                   <h3>Overview of Preload, Create, Update and Render</h3>
                   <p>Phaser has a number of special ‘reserved’ functions that are executed at specific points in the game, and each contain specific pieces of code to perform distinct operations. Preload loads content before creating, updating and rendering your game.</p>
                   <h3>Preload aka Loading Game Assets</h3>
@@ -16,7 +17,7 @@ class Resources extends React.Component {
       		<h3>Creating your Canvas</h3>
                   <p>Canvas creation occurs with every new instantiation of a Phaser game. After preload finishes running, Create is called. Create is where you get to build your game world. Next is update. Update is the heart of your game. This function allows your player to move based on your input, detect collisions and update game state. Lastly, render is only invoked to handle debugging. These essential functions will be described in more depth later on in this tutorial.</p>
                   <br></br>
-                  <h2 id="makeVideo"> Using Phaser's Create Function </h2>
+                  <span id="create"><h2 id="makeVideo"> Using Phaser's Create Function </h2></span>
                   <h3>What is Phaser.Create?</h3>
                   <p>The create function is called automatically once the code inside of your preload function has stopped executing.  Because of this, you can write code in here that has access to all of the assets and other things you loaded in your preload function.  Just like the name suggests, this is the part of your code where you create sprites, groups and many other things that your Phaser game will act on. </p>
                   <h3>How to Create a Sprite</h3>
@@ -27,7 +28,7 @@ One important note: the order that items are rendered depends on the order you c
                   <h3>What are Groups?</h3>
                   <p>A group acts as a container for display objects, which includes things like sprites and images.  Any transformations applied to a group will be applied to all of the objects that are defined as a part of this group.  For example, if you scale up a group, all of the group’s children objects will be scaled up as well.  Groups also make it easier for you to recycle objects with identical properties such as platforms or enemy sprites.  game.add.group() creates the group that we assign to a local variable - so platforms =game.add.group() creates a group that we can refer to as platforms.</p>
             <br></br>
-            <h2 id="makeVideo"> Mastering Phaser's Update Function </h2>
+            <span id="update"><h2 id="makeVideo"> Mastering Phaser's Update Function </h2></span>
             <h3>The Update Function</h3>
             <p>The update function is the core game loop.   It is run every frame of the game.  It usually runs around 60 times per second, with variations due to different computer processors  The update function is responsible for calling both the update logic function (which in turn updates the logic subsystems such as the physics, camera, and sound subsystems) and the updateRender function.</p>
             <h3>But What Does It Really Do?</h3>
@@ -35,6 +36,13 @@ One important note: the order that items are rendered depends on the order you c
             <h3>Yes, the Update Loop is as Cool as it Sounds.</h3>
             <p>Some cool things that you’ll be introduced to over the course of this tutorial that will be in the update loop: moving, jumping, colliding, shooting, scoring, bouncing, swimming, flying, powerups, and a lot more.</p>
             <br></br>
+            </div>
+            <div className="col-md-2">
+                  <h3 id="makeVideo"> Topics </h3>
+                  <a href="#functions"><p>Understanding Phaser's Four Core Functions: Preload, Create, Update and Render</p></a>
+                  <a href="#create"><p>Using Phaser's Create Function</p></a>
+                  <a href="#update"><p>Mastering Phaser's Update Function</p></a>
+            </div>
       	</div>
       </div>
       )
