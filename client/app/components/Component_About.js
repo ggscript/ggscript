b7c0a3ab653bc88ecd6bab5fff6fe76e4cdb6f56
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-11-23 18:45:27 -0800

    add tech stack description

--- client/app/components/Component_About.js
+++ client/app/components/Component_About.js
@@ -61,28 +61,28 @@ class About extends React.Component {
       				<img className="img-responsive text-center" src="http://i.imgur.com/hQaEDFD.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> React/Redux </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">React is an open-source JavaScript library for building user interfaces. It is declarative, efficient and flexible. Redux is a predictable state container for JavaScript applications. It helps you write applications that behave consistently, run in different environments, and are easy to test.</p>
       		</div>
       		<div className="col-md-3">
       			<div className="profile-userpic">
       				<img className="img-responsive text-center" src="http://i.imgur.com/AR2hx4w.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Postgres </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Postgres is a powerful, open-source object-relational database system.</p>
       		</div>
       		<div className="col-md-3">
       			<div className="profile-userpic">
       				<img className="img-responsive text-center" src="http://i.imgur.com/ILucwC8.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Node/Express </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Node is an open-source, cross-platform JavaScript runtime environment for developing applications. Express is a web application framework for Node.</p>
       		</div>
       		<div className="col-md-3">
       			<div className="profile-userpic">
       				<img className="img-responsive text-center" src="http://i.imgur.com/Kiw2aZg.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Phaser </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Phaser is a fun, free and fast 2D game framework for making HTML5 games for desktop and mobile web browsers, supporting Canvas and WebGL rendering.</p>
       		</div>
       	</div>
       	<div className="row">
@@ -91,21 +91,21 @@ class About extends React.Component {
       				<img className="img-responsive text-center" src="http://i.imgur.com/zSP9Pac.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Circle CI </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Circle CI is a modern continuous integration and delivery platform in which developer's changes are immediately tested and reported when they are added to the main code base.</p>
       		</div>
       		<div className="col-md-4">
       			<div className="profile-userpic">
       				<img className="img-responsive text-center" src="http://i.imgur.com/QS2Mv3Q.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center">Jest / Jasmine / Enzyme</h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Jest is a JavaScript testing framework, used by Facebook to test all JavaScript code including React applications. Enzyme is a JavaScript Testing utility for React that makes it easier to assert, manipulate, and traverse React Component's output. Jasmine is a behavior-driven development framework for testing JavaScript code.</p>
       		</div>
       		<div className="col-md-4">
       			<div className="profile-userpic">
       				<img className="img-responsive text-center" src="http://i.imgur.com/TSyhQcu.png"></img>
       			</div>
       			<h2 id="makeVideo" className="text-center"> Webpack </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+      			<p className="text-center">Webpack is a module bundler that takes modules with dependencies and generates static assets representing those modules.</p>
       		</div>
       	</div>
       	<br></br>
