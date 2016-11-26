7fd45d01e656cd588e3e34f54d315e7c785bebac
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-11-25 18:09:09 -0800

    add server tests

--- spec/server/serverspec.js
+++ spec/server/serverspec.js
@@ -11,4 +11,22 @@ describe('/', function () {
       })
       .catch(done.fail);
   });
+  it('should successfully receive response to userdata', function (done) {
+  	request(this.server)
+  		.get('/api/leveldata')
+  		.then((res) => {
+  			expect(res.statusCode).toEqual(200);
+  			done();
+  		})
+  		.catch(done.fail);
+  });
+  it('should successfully receive response to sharedgames', function (done) {
+  	request(this.server)
+  		.get('/api/sharedgames')
+  		.then((res) => {
+  			expect(res.statusCode).toEqual(200);
+  			done();
+  		})
+  		.catch(done.fail);
+  }); 
 });
