b09809588333fca1f015028b46fcfcd05a8f05a6
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-11-27 20:16:22 -0800

    fix merge

--- client/app/components/Component_About.js
+++ client/app/components/Component_About.js
@@ -6,109 +6,109 @@ class About extends React.Component {
     return (
       <div className="container">
             <div id="top"></div>
-      	<div className="row">
+            <div className="row">
             <div className="sandColor">
-      		<h1 id="makeVideo"> About the Team </h1>
+                  <h1 id="makeVideo"> About the Team </h1>
             </div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://lh3.googleusercontent.com/-bIswOe7xSYg/AAAAAAAAAAI/AAAAAAAAAA0/y0_6WkOxIv0/photo.jpg?sz=250"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Jason Maier </h2>
-      			<p className="text-center">I enjoy crafting user experiences or solving problems in interesting ways as much as I enjoy getting exact lethal with tempo mage on Hearthstone. Also, go bears!</p>
-      				<div className="col-md-12 text-center">
-		      			<a href="https://github.com/jason-maier" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/maierjason" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
-		      		</div>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://avatars2.githubusercontent.com/u/20237437?v=3&s=250"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Jeff Judkins </h2>
-      			<p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
-      			<div className="col-md-12 text-center">
-		      			<a href="https://github.com/fej-snikduj" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/1jeffjudkins" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
-		      		</div>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://avatars0.githubusercontent.com/u/16483343?v=3&s=250"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Michelle Li </h2>
-      			<p className="text-center">I love good food and traveling. I enjoy putting together puzzles and reading mystery novels, which is why I enjoy coding.</p>
-      			<div className="col-md-12 text-center">
-		      			<a href="https://github.com/miteaisgreener" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/jing-michelle-li" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
-		      		</div>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="https://avatars2.githubusercontent.com/u/16391503?v=3&s=250"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Nikki Galusha </h2>
-      			<p className="text-center">I am passionate about education and strive to rid the world of problems, regardless of how big or small. On any given weekend, you might find me running around the city or curled up in bed, getting lost in a book.</p>
-      			<div className="col-md-12 text-center">
-		      		<a href="https://github.com/nikkigalusha" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
-		      			<a href="https://www.linkedin.com/in/nikkigalusha" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
-		      		</div>
-      		</div>
-      	</div>
-      	<br></br>
-      	<br></br>
-      	<div className="row">
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="https://lh3.googleusercontent.com/-bIswOe7xSYg/AAAAAAAAAAI/AAAAAAAAAA0/y0_6WkOxIv0/photo.jpg?sz=250"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Jason Maier </h2>
+                        <p className="text-center">I enjoy crafting user experiences or solving problems in interesting ways as much as I enjoy getting exact lethal with tempo mage on Hearthstone. Also, go bears!</p>
+                              <div className="col-md-12 text-center">
+                                    <a href="https://github.com/jason-maier" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+                                    <a href="https://www.linkedin.com/in/maierjason" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+                              </div>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="https://avatars2.githubusercontent.com/u/20237437?v=3&s=250"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Jeff Judkins </h2>
+                        <p className="text-center">I enjoy drawing and making things that bring values or better experiences, and that's what makes coding so appealing to me. Also, currently a jiu-jitsu white belt in training.</p>
+                        <div className="col-md-12 text-center">
+                                    <a href="https://github.com/fej-snikduj" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+                                    <a href="https://www.linkedin.com/in/1jeffjudkins" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+                              </div>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="https://avatars0.githubusercontent.com/u/16483343?v=3&s=250"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Michelle Li </h2>
+                        <p className="text-center">I love good food and traveling. I enjoy putting together puzzles and reading mystery novels, which is why I enjoy coding.</p>
+                        <div className="col-md-12 text-center">
+                                    <a href="https://github.com/miteaisgreener" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+                                    <a href="https://www.linkedin.com/in/jing-michelle-li" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+                              </div>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="https://avatars2.githubusercontent.com/u/16391503?v=3&s=250"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Nikki Galusha </h2>
+                        <p className="text-center">I am passionate about education and strive to rid the world of problems, regardless of how big or small. On any given weekend, you might find me running around the city or curled up in bed, getting lost in a book.</p>
+                        <div className="col-md-12 text-center">
+                              <a href="https://github.com/nikkigalusha" target="_blank"><img className='socialIcon' src="http://i.imgur.com/3x2RfT5.png"></img></a>
+                                    <a href="https://www.linkedin.com/in/nikkigalusha" target="_blank"><img className='socialIcon' src='http://i.imgur.com/Dwt25GE.png'></img></a>
+                              </div>
+                  </div>
+            </div>
+            <br></br>
+            <br></br>
+            <div className="row">
             <div className="teaColor">
-      		<h1 id="makeVideo"> Our Tech Stack </h1>
+                  <h1 id="makeVideo"> Our Tech Stack </h1>
             </div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/hQaEDFD.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> React/Redux </h2>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/AR2hx4w.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Postgres </h2>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/ILucwC8.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Node/Express </h2>
-      		</div>
-      		<div className="col-md-3">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/Kiw2aZg.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Phaser </h2>
-      		</div>
-      	</div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/hQaEDFD.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> React/Redux </h2>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/AR2hx4w.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Postgres </h2>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/ILucwC8.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Node/Express </h2>
+                  </div>
+                  <div className="col-md-3">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/Kiw2aZg.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Phaser </h2>
+                  </div>
+            </div>
             <br></br>
             <br></br>
-      	<div className="row">
-      		<div className="col-md-4">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/zSP9Pac.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Circle CI </h2>
-      		</div>
-      		<div className="col-md-4">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/QS2Mv3Q.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center">Jest / Jasmine / Enzyme</h2>
-      		</div>
-      		<div className="col-md-4">
-      			<div className="profile-userpic">
-      				<img className="img-responsive text-center" src="http://i.imgur.com/TSyhQcu.png"></img>
-      			</div>
-      			<h2 id="makeVideo" className="text-center"> Webpack </h2>
-      		</div>
-      	</div>
-      	<br></br>
+            <div className="row">
+                  <div className="col-md-4">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/zSP9Pac.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Circle CI </h2>
+                  </div>
+                  <div className="col-md-4">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/QS2Mv3Q.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center">Jest / Jasmine / Enzyme</h2>
+                  </div>
+                  <div className="col-md-4">
+                        <div className="profile-userpic">
+                              <img className="img-responsive text-center" src="http://i.imgur.com/TSyhQcu.png"></img>
+                        </div>
+                        <h2 id="makeVideo" className="text-center"> Webpack </h2>
+                  </div>
+            </div>
+            <br></br>
       </div>
       )
   }
